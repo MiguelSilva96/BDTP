8d7ebe87b8183eed7b3e02492ab6ad56e24cbab4
Luís Fernandes <Lus Fernandes>
2016-11-23 18:55:07 +0000

    Povoamento completo! FUNFA DIREITINHO e acrescentei a tabela Lugares que nos tínhamos esquecido

--- Povoamento.sql
+++ Povoamento.sql
@@ -17,6 +17,31 @@ INSERT INTO Comboio
 		(1, 10),
 		(2, 10);
 
+-- Povoamente da tabela "Lugares"
+INSERT INTO Lugares
+	(Lugar, Comboio)
+    VALUES
+		(1, 1),
+        (2, 1),
+        (3, 1),
+        (4, 1),
+        (5, 1),
+        (6, 1),
+        (7, 1),
+        (8, 1),
+        (9, 1),
+        (10, 1),
+        (1, 2),
+        (2, 2),
+        (3, 2),
+        (4, 2),
+        (5, 2),
+        (6, 2),
+        (7, 2),
+        (8, 2),
+        (9, 2),
+        (10, 2);
+
 
 -- Povoamento da tabela "Estação"
 INSERT INTO Estação
