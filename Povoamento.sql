963f1ca88f540cdd41461ab200aecc0153610440
Pedro Cunha <pedrocunha1596@gmail.com>
2016-11-23 16:25:12 +0000

    Povoamento a funcionar

--- Povoamento.sql
+++ Povoamento.sql
@@ -4,22 +4,11 @@ USE IberoTrem;
 INSERT INTO Cliente
 	(CC, Data_de_Nascimento, Nome, Telefone, Email)
 	VALUES 
-		('76452899', 1967-11-12, 'João Bragança', '914934870', 'joao_67_bra@iol.pt'),
-		('83509478', 1974-09-23, 'Fernanda Castro', '929896439', 'nanda123@gmail.com'),
-		('54365476', 1955-08-12, 'Miguel Castro', '93780900', 'miguel_c@hotmail.pt'),
-		('86104398', 1996-12-25, 'Alexandre Mendes', '918626154', 'alex_fcf_96@hotmail.pt'),
-		('55787890A', 1960-11-13, 'Diego Murillo', '917483436', 'diego_murillo@gmail.pt');
-
--- Povoamento da tabela "Reserva"
-INSERT INTO Reserva
-	(Id_reserva, Preço, Lugar, Data, CC, Id_viagem)
-	VALUES 
-		(34, 60.0, 1, 2016-11-01,'76452899', 1),
-		(35, 45.0, 2, 2016-11-01, '86104398', 1),
-		(40, 83.5, 1, 2016-11-05, '83509478', 2),
-		(58, 60.0, 3, 2016-11-07, '54365476', 3),
-		(67, 83.5, 7, 2016-11-10, '55787890A', 4);
-
+		('76452899', '1967-11-12', 'João Bragança', '914934870', 'joao_67_bra@iol.pt'),
+		('83509478', '1974-09-23', 'Fernanda Castro', '929896439', 'nanda123@gmail.com'),
+		('54365476', '1955-08-12', 'Miguel Castro', '93780900', 'miguel_c@hotmail.pt'),
+		('86104398', '1996-12-25', 'Alexandre Mendes', '918626154', 'alex_fcf_96@hotmail.pt'),
+		('55787890A', '1960-11-13', 'Diego Murillo', '917483436', 'diego_murillo@gmail.pt');
 
 -- Povoamento da tabela "Comboio"
 INSERT INTO Comboio
@@ -47,5 +36,18 @@ INSERT INTO Viagem
 		(2, '08:34', '00:02', 83.5, 4, 2, 2),
 		(3, '08:04', '00:12', 60.0, 3, 1, 1),
 		(4, '06:47', '23:17', 83.5, 2, 2, 4);
+
+
+-- Povoamento da tabela "Reserva"
+INSERT INTO Reserva
+	(Id_reserva, Preço, Lugar, Data, CC, Id_viagem)
+	VALUES 
+		(34, 60.0, 1, '2016-11-01','76452899', 1),
+		(35, 45.0, 2, '2016-11-01', '86104398', 1),
+		(40, 83.5, 1, '2016-11-05', '83509478', 2),
+		(58, 60.0, 3, '2016-11-07', '54365476', 3),
+		(67, 83.5, 7, '2016-11-10', '55787890A', 4);
+
+
 
 
